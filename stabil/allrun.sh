4b0ec94f573a472201863993384e360c810d72cb
DotAja <142836390+DotAja@users.noreply.github.com>
2024-10-20 00:27:27 +0700

    Create allrun.sh

---
+++ stabil/allrun.sh
@@ -0,0 +1,7 @@
+wget -O ganti https://raw.githubusercontent.com/DotAja/AUTO-RUNMINER/refs/heads/main/stabil/ganti.sh
+wget -O lun https://raw.githubusercontent.com/DotAja/AUTO-RUNMINER/refs/heads/main/stabil/minel.sh
+wget -O key https://raw.githubusercontent.com/DotAja/AUTO-RUNMINER/refs/heads/main/stabil/key.sh
+
+chmod +x ganti
+chmod +x lun
+chmod +x key
