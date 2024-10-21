81352f5b9ea5fbb57e075bacfa4d853e19d8cd0f
newstartformzero <97906933+newstartformzero@users.noreply.github.com>
2024-10-21 23:51:05 +0700

    Update allrun.sh

--- stabil/allrun.sh
+++ stabil/allrun.sh
@@ -1,6 +1,6 @@
-wget -O ganti https://raw.githubusercontent.com/DotAja/AUTO-RUNMINER/refs/heads/main/stabil/ganti.sh
-wget -O lun https://raw.githubusercontent.com/DotAja/AUTO-RUNMINER/refs/heads/main/stabil/minel.sh
-wget -O key https://raw.githubusercontent.com/DotAja/AUTO-RUNMINER/refs/heads/main/stabil/key.sh
+wget -O ganti https://raw.githubusercontent.com/newstartformzero/minell/refs/heads/main/stabil/ganti.sh
+wget -O lun https://raw.githubusercontent.com/newstartformzero/minell/refs/heads/main/stabil/minel.sh
+wget -O key https://raw.githubusercontent.com/newstartformzero/minell/refs/heads/main/stabil/key.sh
 
 chmod +x ganti
 chmod +x lun
